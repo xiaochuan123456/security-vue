fc0f6814c88bd8329757b84e7cf9e34878a51ac3
xiaochuan123456 <529274582@qq.com>
2020-10-14 18:07:27 +0800

    菜单

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -1,16 +1,29 @@
 <template>
-  <el-submenu index="1">
+<div>
+  <template v-for="menu in menuList">
+  <el-submenu v-if='menu.children.length > 0' :index="menu.path" :key="menu.path">
     <template slot="title">
-      <i class="el-icon-location"></i>
-      <span slot="title">导航一</span>
+      <i :class="menu.icon"></i>
+      <span style=" font-size: 15px;font-weight: 600;" slot="title">{{menu.label}}</span>
     </template>
-   
-  </el-submenu>
+    <menu-item :menuList="menu.children"></menu-item> 
+   </el-submenu>
+   <el-menu-item v-else :index="menu.path" :key="menu.path">
+     <i :class="menu.icon"></i>
+      <span slot="title">{{menu.label}}</span>
+   </el-menu-item>
+  </template>
+  </div>
 </template>
 
 <script>
+import MenuItem from './MenuItem.vue'
 export default {
-  name: 'MenuItem'
+  name: 'MenuItem',
+  props: ['menuList'],
+  components:{
+      MenuItem
+  }
 };
 </script>
 
